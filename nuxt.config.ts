705639fb464ce4dc9c16bfca843a62b6cfa1d837
nehas <nehas@proximabiz.com>
2023-12-19 21:40:14 +0530

    Eslint & Prettier setup completed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: {
+    enabled: true
+  },
+  // https://nuxt.com/modules
+  modules: ['@nuxtjs/eslint-module'],
+  // https://nuxt.com/modules/eslint
+  eslint: {
+    cache: true,
+    formatter: 'stylish',
+    lintOnStart: true,
+    emitWarning: true,
+    emitError: true,
+    failOnWarning: true,
+    failOnError: false
+  }
+});
