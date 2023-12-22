85b6bb0cec138f8448a46a917fa9058bd457fb8d
Jeet-Metapercept <jeetp@proximabiz.com>
2023-12-22 05:56:10 +0530

    examples

--- app.vue
+++ app.vue
@@ -9,6 +9,49 @@ const isDark = computed({
     colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
   },
 })
+
+const countries = ['India', 'United States', 'Canada', 'Mexico']
+const country = ref(countries[0])
+const people = ['Wade Cooper', 'Arlene Mccoy', 'Devon Webb', 'Tom Cook', 'Tanya Fox', 'Hellen Schmidt', 'Caroline Schultz', 'Mason Heaney', 'Claudie Smitham', 'Emil Schaefer']
+const selected = ref([])
+
+const peoples = [{
+  id: 1,
+  name: 'Lindsay Walton',
+  title: 'Front-end Developer',
+  email: 'lindsay.walton@example.com',
+  role: 'Member',
+}, {
+  id: 2,
+  name: 'Courtney Henry',
+  title: 'Designer',
+  email: 'courtney.henry@example.com',
+  role: 'Admin',
+}, {
+  id: 3,
+  name: 'Tom Cook',
+  title: 'Director of Product',
+  email: 'tom.cook@example.com',
+  role: 'Member',
+}, {
+  id: 4,
+  name: 'Whitney Francis',
+  title: 'Copywriter',
+  email: 'whitney.francis@example.com',
+  role: 'Admin',
+}, {
+  id: 5,
+  name: 'Leonard Krasner',
+  title: 'Senior Designer',
+  email: 'leonard.krasner@example.com',
+  role: 'Owner',
+}, {
+  id: 6,
+  name: 'Floyd Miles',
+  title: 'Principal Designer',
+  email: 'floyd.miles@example.com',
+  role: 'Member',
+}]
 </script>
 
 <template>
@@ -45,10 +88,24 @@ const isDark = computed({
         </ClientOnly>
       </UCard>
 
+      <div class="m-10 p-7 border rounded-md shadow-sm">
+        <h2 class="mb-4">
+          Selects
+        </h2>
+        <div class="grid grid-cols-2 gap-4">
+          <USelect v-model="country" :options="countries" />
+          <USelectMenu
+            v-model="selected" :options="people" multiple searchable
+            searchable-placeholder="Search a person..." :padded="true" placeholder="Select people"
+          />
+        </div>
+      </div>
+
       <UCard class="m-10">
         <template #header>
-          <h2>Date Picker</h2>
+          <h2>Table</h2>
         </template>
+        <UTable :rows="peoples" />
       </UCard>
     </UContainer>
   </div>
