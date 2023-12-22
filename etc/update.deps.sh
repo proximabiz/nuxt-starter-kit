bfc79aa709f58cfe95335a8b284bea6aaa92fe83
nehas <nehas@proximabiz.com>
2023-12-22 13:56:09 +0530

    added-doctor-command-to-warn-id-dependency-mismatch & removed-default-value-from-npmrc

--- etc/update.deps.sh
+++ etc/update.deps.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+# Ref- https://www.npmjs.com/package/npm-check-updates/v/11.3.0
+
 # check for updates, and upgrade if requested
-echo "${packages}" | awk '{print $2 "package.json"}' | xargs -n 1 -x ncu "${@}" --packageFile
+echo "${packages}" | awk '{print $2 "package.json"}' | xargs -n 1 -x ncu --doctor -u "${@}"
 
 # we upgraded our package.json files,Install dependencies using pnpm
 echo "Installing dependencies with pnpm..."
