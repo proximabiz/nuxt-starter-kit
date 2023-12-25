96fddd066892b2ad1c22be45e56ce762c7f2b3ae
Jeet-Metapercept <jeetp@proximabiz.com>
2023-12-25 15:20:48 +0530

    Update bundle.size.sh

--- etc/bundle.size.sh
+++ etc/bundle.size.sh
@@ -6,31 +6,39 @@ GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 NC='\033[0m'
 
-list_files_and_folders_with_size() {
+list_files_with_size() {
     local total_size=0
     local entry_size
 
     echo "➜ Analyzing build..."
     echo ""
 
+    format_size() {
+        du -sh "$1" | cut -f1
+    }
+
     for entry in "$OUTPUT_DIR"/*; do
         if [[ -f "$entry" ]]; then
-            entry_size=$(du -sh "$entry" | cut -f1)
-            echo "- ${GREEN}$(basename "$entry")${NC} - Size: $entry_size"
+            entry_size=$(format_size "$entry")
+            echo "  ├─ ${GREEN}$entry${NC} ($entry_size)"
         elif [[ -d "$entry" ]]; then
-            entry_size=$(du -sh "$entry" | cut -f1)
-            echo "- ${RED}$(basename "$entry")${NC} - Size: $entry_size"
+            echo "  ├─ ${RED}$entry${NC}"
+            for subentry in "$entry"/*; do
+                if [[ -f "$subentry" ]]; then
+                    entry_size=$(format_size "$subentry")
+                    echo "  │  ├─ $(basename "$subentry") ($entry_size)"
+                fi
+            done
         fi
-        total_size=$(($total_size + $(du -s "$entry" | cut -f1)))
     done
 
     total_size_hr=$(du -sh -c "$OUTPUT_DIR" | grep 'total$' | cut -f1)
     echo ""
-    echo "➜ ${YELLOW}Total size of entries in $OUTPUT_DIR: $total_size_hr${NC}"
+    echo "➜ ${YELLOW}Total bundle size: $total_size_hr${NC}"
 }
 
 if [[ -d "$OUTPUT_DIR" ]]; then
-    list_files_and_folders_with_size
+    list_files_with_size
 else
     echo "Error: Directory $OUTPUT_DIR does not exist."
     exit 1
