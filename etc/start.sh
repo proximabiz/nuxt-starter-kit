7b47f9cd8e57ebf1a5fea469ade20ab82e2d9c7a
Neha Soni <145762828+neha-proxima@users.noreply.github.com>
2023-12-26 12:57:47 +0530

    Local server start script for quick setup (#10)

---
+++ etc/start.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Check if the OS is Mac or Linux
+if [[ "$OSTYPE" != "darwin"* && "$OSTYPE" != "linux"* ]]; then
+    echo "This script is intended for Mac or Linux systems only."
+    exit 1
+fi
+
+# Check if Node.js is installed
+if ! command -v node &> /dev/null; then
+    echo "Node.js is not installed. Please install Node.js before running the application."
+    exit 1
+fi
+
+# Check if npm is installed
+if ! command -v npm &> /dev/null; then
+    echo "npm is not installed. Please install npm before running the application."
+    exit 1
+fi
+
+# Check if pnpm is installed, if not, install it
+if ! command -v pnpm &> /dev/null; then
+    echo "pnpm is not installed. Installing pnpm..."
+    npm install -g pnpm
+fi
+
+# Install dependencies using pnpm
+echo "Installing dependencies using pnpm..."
+pnpm install
+
+# Check if .env file exists, if not, create it and copy example.env content
+if [ ! -f .env ]; then
+    echo "Creating .env file and copying example.env content..."
+    cp example.env .env
+fi
+
+echo "Setup Done"
+echo "Now you can start the server using \"pnpm dev\" command"
