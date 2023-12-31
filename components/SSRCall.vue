3537b88c390542b9143fd5e38b5cc8a46c36bbbc
Jeet-Metapercept <jeetp@proximabiz.com>
2023-12-31 15:12:21 +0530

    Update SSRCall.vue

--- components/SSRCall.vue
+++ components/SSRCall.vue
@@ -30,7 +30,7 @@ const { data: products, pending } = useFetch<Array<Product>>('https://fakestorea
     please wait...
   </div>
   <div v-else class="bg-white">
-    <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+    <div class="mx-auto max-w-2xl px-4 py-6 sm:px-6 sm:py-12 lg:max-w-7xl lg:px-8">
       <div class="flex justify-between">
         <h2 class="text-2xl font-bold tracking-tight text-gray-900">
           Products
