ff46f5daab6cfb28a58334eaa5e4e8462127c731
Jeet-Metapercept <jeetp@proximabiz.com>
2024-01-01 15:16:34 +0530

    Update Elements.vue

--- components/Elements.vue
+++ components/Elements.vue
@@ -23,18 +23,18 @@ function resetElements() {
   <div>
     <div class="py-6">
       <div class="flex gap-2 justify-start pb-4">
-        <UButton icon="lucide:refresh-ccw" label="reset" @click="resetElements()" />
-        <UButton icon="lucide:shuffle" color="gray" @click="shuffleElements()" />
+        <UButton icon="lucide:refresh-ccw" variant="outline" size="sm" @click="resetElements()" />
+        <UButton icon="lucide:shuffle" variant="outline" size="sm" @click="shuffleElements()" />
       </div>
       <!-- <div v-auto-animate class="flex flex-wrap gap-4">
         <UButton v-for="el in elements" :key="el.number" size="sm" :label="el.symbol" color="gray" variant="solid" class="min-w-12 min-h-12 justify-center hover:shadow-md hover:scale-125" />
       </div> -->
       <div v-auto-animate class="flex flex-wrap gap-2">
-        <UTooltip v-for="el in elements" :key="el.number">
-          <UButton :label="el.symbol" color="gray" variant="solid" size="sm" class="min-w-12 min-h-12 justify-center" />
+        <UTooltip v-for="el in elements" :key="el.number" :shortcuts="[`${el.number}`]">
+          <UButton :label="el.symbol" color="gray" variant="solid" class=" min-w-12 min-h-12 justify-center" />
 
           <template #text>
-            <span class="font-semibold">{{ el.name }} ({{ el.number }}) </span>
+            <span class="font-medium">{{ el.name }}</span>
           </template>
         </UTooltip>
       </div>
