cbc789f4d6eb994174fc84f08d3a01168519d8ea
Jeet-Metapercept <jeetp@proximabiz.com>
2024-01-01 15:40:15 +0530

    Update app.vue

--- app.vue
+++ app.vue
@@ -97,7 +97,7 @@ function shuffleItems() {
       <div class="mx-auto px-4 sm:px-6 lg:px-8 gap-16 sm:gap-y-24 max-w-4xl flex flex-col">
         <div class="relative z-[1] text-center">
           <h1 class="text-5xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-7xl">
-            <span>A <span class="text-primary">Nuxt Starter Kit</span> for<br class="hidden lg:block"> Modern Web
+            <span>A <span class="text-primary animate-pulse">Nuxt Starter Kit</span> for<br class="hidden lg:block"> Modern Web
               Apps</span>
           </h1>
           <p class="mt-6 text-lg tracking-tight text-gray-600 dark:text-gray-300">
