30d1fbeedeccc57f040b8a37e2a62deb0fd74dc9
Swapnil Jagtap <sjagtap@contractor.linuxfoundation.org>
2024-02-08 20:00:20 +0530

    Changes odne

--- server/api/diagram/versions.get.ts
+++ server/api/diagram/versions.get.ts
@@ -4,8 +4,9 @@ import { serverSupabaseClient } from '#supabase/server'
 export default defineEventHandler(async (event) => {
   await protectRoute(event)
   const userId = event.context.user._id
+  const diagramId: string = getRouterParam(event, 'id')!
   const client = await serverSupabaseClient(event)
-  const { data, error, status } = await client.from('diagram_version').select('*').eq('user_id', userId).select()
+  const { data, error, status } = await client.from('diagram_version').select('*').eq('user_id', userId).eq('diagram_id', diagramId).select()
 
   return {
     data,
