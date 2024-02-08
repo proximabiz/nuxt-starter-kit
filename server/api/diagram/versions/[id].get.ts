421ddee1bc1dc451ccb9449bb83a16a6eaca3007
Swapnil Jagtap <sjagtap@contractor.linuxfoundation.org>
2024-02-08 20:16:17 +0530

    Versions API Done

--- server/api/diagram/versions/[id].get.ts
+++ server/api/diagram/versions/[id].get.ts
@@ -1,9 +1,9 @@
-import { protectRoute } from '../../utlis/route.protector'
+import { protectRoute } from '../../../utlis/route.protector'
 import { serverSupabaseClient } from '#supabase/server'
 
 export default defineEventHandler(async (event) => {
   await protectRoute(event)
-  const userId = event.context.user._id
+  const userId = event.context.user.id
   const diagramId: string = getRouterParam(event, 'id')!
   const client = await serverSupabaseClient(event)
   const { data, error, status } = await client.from('diagram_version').select('*').eq('user_id', userId).eq('diagram_id', diagramId).select()
