86d436e56efa1545229bde654e27273e78a077ed
ipsita <ipsitap@proximabiz.com>
2024-02-14 18:19:15 +0530

    added the active nav bar color

--- components/layout/WebsiteTopBar.vue
+++ components/layout/WebsiteTopBar.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+const route = useRoute()
 const notify = useNotification()
 const authStore = useAuthStore()
 const supabaseClient = useSupabaseClient()
@@ -45,6 +46,9 @@ async function singOut() {
     notify.error(error)
   }
 }
+function isActiveRoute(to: string) {
+  return route.path === to
+}
 </script>
 
 <template>
@@ -60,9 +64,10 @@ async function singOut() {
         </svg>
       </button>
     </div>
-    <ul class="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:flex lg:items-center lg:w-auto lg:space-x-6">
+    <ul class="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:items-center lg:w-auto lg:space-x-6">
       <template v-for="(item, index) in links" :key="index">
-        <li><a class="text-sm text-gray-400 hover:text-gray-500 cursor-pointer" @click="navigateTo(item.to)">{{ item.name }}</a></li>
+
+        <li><a class="text-sm text-gray-400 hover:text-gray-500 cursor-pointer" :class="{ 'active-link': isActiveRoute(item.to) }" @click="navigateTo(item.to)" >{{ item.name }}</a></li>
         <li class="text-gray-300">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" class="w-4 h-4 current-fill" viewBox="0 0 24 24">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
@@ -135,7 +140,7 @@ async function singOut() {
       </div>
       <div class="mt-auto">
         <div class="pt-6">
-          <a class="block px-4 py-3 mb-3 leading-loose text-xs text-center font-semibold leading-none bg-gray-50 hover:bg-gray-100 rounded-xl" href="#">Sign in</a>
+          <a class="block px-4 py-3 mb-3 leading-loose text-xs text-center font-semibold bg-gray-50 hover:bg-gray-100 rounded-xl" href="#">Sign in</a>
           <a class="block px-4 py-3 mb-2 leading-loose text-xs text-center text-white font-semibold bg-blue-600 hover:bg-blue-700  rounded-xl" href="#">Sign Up</a>
         </div>
         <p class="my-4 text-xs text-center text-gray-400">
