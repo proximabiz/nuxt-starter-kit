5e7423cda8f668ad95ee4d3f2a87879016c70f93
pushpak-hazare <pushpakh@proximabiz.com>
2024-02-14 10:30:37 +0530

    code fixes for upgrade popup

--- app.vue
+++ app.vue
@@ -7,41 +7,54 @@ const route = useRoute()
 const authUser = computed(() => authStore.getAuthUser.value)
 const planStore = useBillingStore()
 
-const showUpgradeModal=ref<boolean>(false)
+const showUpgradeModal = ref<boolean>(false)
 
 watch(
   () => authUser.value,
   async (user) => {
-    if (!user && !route.fullPath.includes('/login')) {
-      navigateTo('/login');
-    }
-    if (user && route.fullPath.includes('/login')) {
-      navigateTo('/');
-    }
+    if (!user && !route.fullPath.includes('/login'))
+      navigateTo('/login')
+
+    if (user && route.fullPath.includes('/login'))
+      navigateTo('/')
+
     if (user?.id) {
-      const response = await planStore.fetchActivePlan();
-      if (response?.subscription_status === "PLAN_EXPIRED") {
-        showUpgradeModal.value=true
-       
-      } else if (response.subscription_status === "NO_SUBSCRIPTION") {
-        let payload = {
-          userId: user.id, 
-          subscriptionTypeId: "10dbc647-04ea-4588-b6c8-7c535049f18c",
-          amount: 0 
-        };
-        if (!route.fullPath.includes('/profile/account')) {
-          await planStore.addSubscription(payload);
+      const response = await planStore.fetchActivePlan()
+      if (response?.subscription_status === 'PLAN_EXPIRED') {
+        showUpgradeModal.value = true
+      }
+      else if (response.subscription_status === 'NO_SUBSCRIPTION') {
+        const payload = {
+          userId: user.id,
+          subscriptionTypeId: '10dbc647-04ea-4588-b6c8-7c535049f18c',
+          amount: 0,
         }
+        if (!route.fullPath.includes('/profile/account'))
+          await planStore.addSubscription(payload)
       }
     }
   },
-  { immediate: true }
-);
-
+  { immediate: true },
+)
+function upgradePlan() {
+  showUpgradeModal.value = false
+  navigateTo('/website/pricing')
+}
 </script>
 
-<template> 
-  <Upgrade :isOpen="showUpgradeModal" @update:is-open="showUpgradeModal"/>
+<template>
+  <UModal :model-value="showUpgradeModal" :transition="false">
+    <div class="p-8">
+      <div class="mb-8">
+        Your plan has expired!...
+      </div>
+      <div class="mt-4 flex justify-end gap-4">
+        <UButton class="" color="gray" @click="upgradePlan">
+          Upgrade
+        </UButton>
+      </div>
+    </div>
+  </UModal>
   <NuxtLayout>
     <NuxtPage />
     <UNotifications />
--- components/Upgrade.vue
+++ components/Upgrade.vue
@@ -2,30 +2,30 @@
 interface Props {
   isOpen: boolean
 }
-interface Emit {
-  (e: 'update:isOpen', value: boolean): void
+// interface Emit {
+//   (e: 'update:isOpen', value: boolean): void
 
-}
+// }
 const props = defineProps<Props>()
-const emit = defineEmits<Emit>();
-const showModal=ref(true)
+// const emit = defineEmits<Emit>()
+const showModal = ref(props.isOpen)
 
-function upgradePlan() { 
-    showModal.value=false
-  emit('update:isOpen', false)
-  navigateTo("/website/pricing")
+function upgradePlan() {
+  showModal.value = false
+  // emit('update:isOpen', false)
+  navigateTo('/website/pricing')
 }
 </script>
 
 <template>
-  <UModal :model-value="props.isOpen && showModal" :transition="false">
+  <UModal :model-value="props.isOpen" :transition="false">
     <div class="p-8">
       <div class="mb-8">
-       Your plan has exparied!...
+        Your plan has expired!...
       </div>
-      <div class="mt-4 flex justify-end gap-4">        
+      <div class="mt-4 flex justify-end gap-4">
         <UButton class="" color="gray" @click="upgradePlan">
-        Upgrade
+          Upgrade
         </UButton>
       </div>
     </div>
