0631a46fe867696e346086940678cfe4365db580
ipsita <ipsitap@proximabiz.com>
2024-02-20 18:20:52 +0530

    Added disable and tooltip to the billing and payment card

--- components/profile/MyAccount.vue
+++ components/profile/MyAccount.vue
@@ -3,15 +3,20 @@ import address from '@/assets/media/address.png'
 import card from '@/assets/media/card.png'
 import tax from '@/assets/media/gst.png'
 import plan from '@/assets/media/plan.png'
+import { useBillingStore } from '~/stores/subscription';
 
 // Refs
 const selectedComponent = ref()
+const subStatusStore = useBillingStore()
+
+const sub_status = computed(() => subStatusStore.GET_SUB_STATUS)
+console.log(sub_status.value.planName)
 
 const cards = [
-  { id: 1, text: 'Address, Contact Details', iconSrc: address, componentName: 'ProfileAddressDetails' },
-  { id: 2, text: 'Tax ID / GST Details', iconSrc: tax, componentName: 'ProfileGstAndTax' },
-  { id: 3, text: 'Billing & Payments', iconSrc: card, componentName: '' },
-  { id: 4, text: 'My Plan', iconSrc: plan, componentName: 'ProfileMyPlan' },
+  { id: 1, text: 'Address, Contact Details', iconSrc: address, componentName: 'ProfileAddressDetails',isDisabled:false },
+  { id: 2, text: 'Tax ID / GST Details', iconSrc: tax, componentName: 'ProfileGstAndTax' ,isDisabled:false},
+  { id: 3, text: 'Billing & Payments', iconSrc: card, componentName: '',isDisabled: sub_status.value.planName==='Free'},
+  { id: 4, text: 'My Plan', iconSrc: plan, componentName: 'ProfileMyPlan',isDisabled:false },
 ]
 
 async function setComponentName(componentName: string) {
@@ -39,10 +44,11 @@ function resetComponent() {
     <div class="grid grid-cols-2 gap-10 mt-20">
       <UCard
         v-for="item in cards" :key="item.id"
-        class="minimum-width hover:bg-gray-300 delay-150 cursor-pointer sm:min-w-20"
-        @click="setComponentName(item.componentName)"
-      >
-        <div class="flex items-center">
+        class="minimum-width sm:min-w-20"
+        :class="item.isDisabled ?'cursor-not-allowed opacity-60':'hover:bg-gray-300 delay-150 cursor-pointer'"
+        @click="setComponentName(item.componentName)" 
+        :title="item.isDisabled ? 'This feature is not available for Free Subscription' : ''">
+        <div class="flex items-center" :title="item.isDisabled ? 'This feature is not available for Free Subscription' : ''">
           <div>
             <img :src="item.iconSrc" class="h-10" alt="">
           </div>
