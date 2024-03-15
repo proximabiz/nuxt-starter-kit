df73c2e001e43b387093ec882245c896d55f8cff
nehas <nehas@proximabiz.com>
2024-03-15 13:22:28 +0530

    netlify-toml-updated-&-package-json-updated

--- composables/notification.ts
+++ composables/notification.ts
@@ -1,22 +1,19 @@
-import type { NotificationColor } from '@nuxt/ui/dist/runtime/types'
-
 export function useNotification() {
   const toast = useToast()
 
-  const addToast = (message: string, color: NotificationColor) => {
-    toast.add({
-      title: message,
-      color,
-    })
-  }
-
   const methods = {
     success: (message: string) => {
-      addToast(message, 'green')
+      toast.add({
+        title: message,
+        color: 'green',
+      })
     },
 
     error: (message: string) => {
-      addToast(message, 'red')
+      toast.add({
+        title: message,
+        color: 'red',
+      })
     },
   }
 
