f2809967ff0ed2f8fc0f53c2388b560b5e3985d8
Jeet-Metapercept <jeetp@proximabiz.com>
2024-03-16 23:04:02 +0530

    Update scripts

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,14 @@
 
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 678161666670.dkr.ecr.ap-south-1.amazonaws.com
 
+sleep 1
+
 docker stack deploy -c docker-compose.production.yml flowmapperai --with-registry-auth
 
+sleep 1
+
 docker system prune -af
+
+sleep 1
 
 echo "Done."
