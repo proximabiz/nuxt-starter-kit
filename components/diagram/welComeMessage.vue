12cd93ce2f4159a6e0be159d87c92ed04714042e
ipsita <ipsitap@proximabiz.com>
2024-03-21 19:57:38 +0530

    added welcome page for new user and added remaining days for free plan

---
+++ components/diagram/welComeMessage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="text-center text-xl font-bold my-8">Begin your Flow Mapper Journey Here</div>
+    <section class="mx-auto max-w-4xl p-6 rounded-lg shadow-md">
+        <p class="text-lg text-gray-800">Welcome aboard! We're thrilled to have you join our community of creative thinkers and visual communicators. Whether you're a seasoned pro or just getting started with diagram creation, you're in the right place.</p>
+        <p class="mt-4 text-lg text-gray-800">At AI FlowMapper, we offer a diverse array of tools and features to help you bring your ideas to life. From mind maps to flowcharts, organizational charts to network diagrams, our platform empowers you to visualize concepts, streamline processes, and express your thoughts with clarity and precision.</p>
+        <p class="mt-4 text-lg font-semibold text-gray-900">Here's what you can expect from your journey with us:</p>
+        <ul class="list-disc pl-8 mt-2 text-lg text-gray-800">
+            <li class="mt-2 bg-blue-100 p-2 border-l-4 border-blue-500"><span class="font-bold">Intuitive Interface:</span> Our user-friendly interface makes it easy to navigate and create diagrams without any hassle. No steep learning curves here – just seamless diagramming at your fingertips.</li>
+            <li class="mt-2 bg-blue-100 p-2 border-l-4 border-blue-500"><span class="font-bold">Versatile Templates:</span> Explore our extensive collection of templates designed to suit various needs and industries. Whether you're planning a project, mapping out strategies, or illustrating concepts, we have templates tailored to your requirements.</li>
+            <li class="mt-2 bg-blue-100 p-2 border-l-4 border-blue-500"><span class="font-bold">Collaboration Tools:</span> Collaboration is key to success, and that's why we've integrated collaborative features into our platform. Invite team members, share your diagrams, and work together in real-time to achieve your goals efficiently.</li>
+            <li class="mt-2 bg-blue-100 p-2 border-l-4 border-blue-500"><span class="font-bold">Export and Sharing Options:</span> Once your masterpiece is ready, you can export it in multiple formats or share it directly with colleagues, clients, or friends. Spread your ideas far and wide with just a few clicks.</li>
+        </ul>
+        <p class="mt-4 text-lg text-gray-800">We're excited to see the incredible diagrams you'll create and the impact they'll have. So why wait? Dive in, unleash your creativity, and let your ideas take shape with AI FlowMapper.</p>
+    </section>
+</template>
