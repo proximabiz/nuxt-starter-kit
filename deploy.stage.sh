f838365c0eafbcb8e981f6ccc4d28dc8276e0ff1
Jeet-Metapercept <jeetp@proximabiz.com>
2024-05-04 00:55:42 +0530

    re-deploy

--- deploy.stage.sh
+++ deploy.stage.sh
@@ -1,46 +1,12 @@
 #!/bin/bash
 STACK="flowmapperai"
 
-# Login to AWS ECR
-echo "Logging into AWS ECR..."
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 678161666670.dkr.ecr.ap-south-1.amazonaws.com
-if [ $? -ne 0 ]; then
-    echo "Failed to login to Docker registry."
-    exit 1
-fi
-
-echo "Login successful. Please wait..."
 sleep 1
-
-# Source environment variables
-if [ -f "./deploy.aws.env" ]; then
-    echo "Sourcing environment variables from deploy.aws.env..."
-    source ./deploy.aws.env
-    if [ $? -ne 0 ]; then
-        echo "Failed to source environment variables."
-        exit 1
-    fi
-else
-    echo "Error: deploy.aws.env file does not exist."
-    exit 1
-fi
-
-# Deploy using Docker
-echo "Deploying Docker stack..."
-docker stack deploy --with-registry-auth -c ./docker-compose.stage.yml $STACK
-if [ $? -ne 0 ]; then
-    echo "Failed to deploy stack."
-    exit 1
-fi
-
-sleep 3
-
-# Clean up Docker system
-echo "Cleaning up Docker system..."
+echo "please wait..."
+sleep 1
+source ./deploy.aws.env && docker stack deploy --with-registry-auth -c ./docker-compose.stage.yml $STACK && sleep 3
+sleep 1
 docker system prune -af
-if [ $? -ne 0 ]; then
-    echo "Failed to prune Docker system."
-    exit 1
-fi
-
-echo "Deployment completed successfully. Done."
+sleep 1
+echo "Done."
