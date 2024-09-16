5057e0c78af3ec20fbc7a17a02272709ae40b143
Neha Soni <neha@Nehas-MacBook-Pro.local>
2024-09-16 13:58:05 +0530

    disabled-the-maps-menu-for-paid-user-special-case

--- components/layout/AppDrawer.vue
+++ components/layout/AppDrawer.vue
@@ -3,16 +3,28 @@ const route = useRoute()
 const { $error } = useNuxtApp()
 const supabaseClient = useSupabaseClient()
 const subscriptionStore = useSubscriptionStore()
+
 const cardDetails = computed(() => subscriptionStore.billingDetails)
 const currentRoutePath = computed(() => route.fullPath)
 const sub_status = computed(() => subscriptionStore.subscriptionStatus)
-const isCardEmtpy = ref<boolean>(true)
+const isPaidUser = computed(() => sub_status.value.planName !== 'Free')
+const isCardDetailsMissing = computed(() => {
+  const { cardHolderName, cardNo, expDate, cvv } = cardDetails.value
+  return !cardHolderName || !cardNo || !expDate || !cvv
+})
 
 const links = computed(() => [
+  {
+    title: 'My diagrams',
+    to: '/app/diagram/list',
+    icon: 'i-heroicons-map',
+    isDisabled: isPaidUser.value && isCardDetailsMissing.value,
+  },
   {
     title: 'My Account',
     to: '/profile/account',
     icon: 'i-heroicons-user',
+    isDisabled: false,
   },
 ])
 async function singOut() {
@@ -26,16 +38,6 @@ async function singOut() {
     $error(error.data.message)
   }
 }
-watch([cardDetails.value, isCardEmtpy.value], () => {
-  const { cardHolderName, cardNo, expDate, cvv } = cardDetails.value
-  if (cardHolderName
-    && cardNo
-    && expDate
-    && cvv ||sub_status.value.planName==='Free')
-    isCardEmtpy.value = false
-  else
-    isCardEmtpy.value = true
-}, { deep: true, immediate: true })
 </script>
 
 <template>
@@ -43,53 +45,32 @@ watch([cardDetails.value, isCardEmtpy.value], () => {
     <div>
       <div class="inline-flex h-16 w-16 items-center justify-center">
         <NuxtLink to="/website">
-          <span
-            class="grid h-10 w-10 place-content-center rounded-lg text-xs text-gray-600"
-          >
+          <span class="grid h-10 w-10 place-content-center rounded-lg text-xs text-gray-600">
             <img src="/assets/media/logo.png" class="" alt="Logo">
           </span>
         </NuxtLink>
       </div>
       <div class="border-t border-gray-100">
         <div class="px-2">
-          <div v-if="!isCardEmtpy" class="py-4 ">
-            <NuxtLink
-              to="/app/diagram/list"
-              class="cursor-pointer"
-            >
-              <a class="t group relative flex justify-center rounded px-2 py-1.5 " :class="[{ 'text-blue-700 bg-blue-100': currentRoutePath === '/app/diagram/list' }]">
-                <UIcon name="i-heroicons-map" class="size-5" />
-                <span class="absolute start-full top-1/2 ms-4 -translate-y-1/2 rounded bg-gray-900 px-2 py-1.5 text-xs font-medium text-white invisible group-hover:visible">
-                  My Diagrams
-                </span>
-              </a>
-            </NuxtLink>
-          </div>
-          <div v-else class="py-4 cursor-not-allowed">
-            <span class="cursor-not-allowed text-gray-400 pointer-events-none">
-              <a class="t group relative flex justify-center rounded px-2 py-1.5 " :class="[{ 'text-blue-700 bg-blue-100': currentRoutePath === '/app/diagram/list' }]">
-                <UIcon name="i-heroicons-map" class="size-5" />
-                <span class="absolute start-full top-1/2 ms-4 -translate-y-1/2 rounded bg-gray-900 px-2 py-1.5 text-xs font-medium text-white invisible group-hover:visible">
-                  My Diagrams
-                </span>
-              </a>
-            </span>
-          </div>
-
           <ul class="space-y-1 border-t border-gray-100 pt-4">
             <li v-for="(item, index) in links" :key="index">
-              <NuxtLink :to="item.to">
-                <a
-                  class="t group relative flex justify-center rounded px-2 py-1.5" :class="[{ 'text-blue-700 bg-blue-100': currentRoutePath === item.to }]"
-                >
-                  <UIcon :name="item.icon" class="size-5" />
-                  <span
-                    class="absolute start-full top-1/2 ms-4 -translate-y-1/2 rounded bg-gray-900 px-2 py-1.5 text-xs font-medium text-white invisible group-hover:visible"
-                  >
-                    {{ item.title }}
-                  </span>
-                </a>
-              </NuxtLink>
+              <div class="border-t border-gray-100">
+                <div class="py-2">
+                  <NuxtLink :to="item.isDisabled ? '' : item.to">
+                    <a
+                      class="t group relative flex justify-center rounded px-2 py-1.5"
+                      :class="[{ 'text-blue-700 bg-blue-100': currentRoutePath === item.to, 'cursor-not-allowed': item.isDisabled }]"
+                    >
+                      <UIcon :name="item.icon" class="size-5" />
+                      <span
+                        class="absolute start-full top-1/2 ms-4 -translate-y-1/2 rounded bg-gray-900 px-2 py-1.5 text-xs font-medium text-white invisible group-hover:visible"
+                      >
+                        {{ item.title }}
+                      </span>
+                    </a>
+                  </NuxtLink>
+                </div>
+              </div>
             </li>
           </ul>
         </div>
